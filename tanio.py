f71cc561d0a44620da9c6077d0dcc44fb84a17fb
Tanio <thanh.tran159@hcmut.edu.vn>
2022-10-27 20:35:14 +0700

    add tanio.py and delete file

---
+++ tanio.py
@@ -0,0 +1,11 @@
+adj1 = input("Nhap vao tinh tu ")
+adj2 = input("Nhap vao tinh tu ")
+verb1 = input("Nhap vao dong tu ")
+noun = input("Nhap vao danh tu ")
+
+
+madlibs = f"In 2025 I will go {adj1}, maybe to some European country but I hope \
+to {verb1} America, because that's my dream country. Though {adj2} but the more \
+the risk, the more the {noun}."
+ 
+print(madlibs)
